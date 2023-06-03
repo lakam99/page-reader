bd25fe2b8dd6fdeca7e998071f1eeafd12629ae1
Arkam Mazrui <arkam.mazrui@smilecdr.com>
2023-06-02 22:04:28 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+//@ts-check
+const parse = require('node-html-parser').parse;
+const fetch = (async () => await (await import('node-fetch')).default)();
+
+const page_reader = (function () {
+    this.config = {'api-key': undefined};
+    /**
+     * 
+     * @param {string | undefined} apiKey 
+     */
+    this.configure = function(apiKey) {
+        // @ts-ignore
+        if (apiKey) this.config["api-key"] = apiKey;
+    }
+    /**
+     * 
+     * @param {string} url 
+     */
+    this.get_page_text = async function(url) {
+        const raw_html = await (await (await fetch)(url)).text();
+        const page = parse(raw_html);
+        let body = page.querySelector('body');
+        body?.querySelectorAll('script, style').forEach((e) => e.parentNode.removeChild(e));
+        console.log(body?.innerText);
+    }
+
+    return {summarise_page: this.get_page_text};
+})();
+
+page_reader.summarise_page("https://youtube.com");
